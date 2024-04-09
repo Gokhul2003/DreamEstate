3c575c947f80a3d7b6b9870b66f5a7fb44b861ec
Gokhul2003 <thegokhul.kooranchi@gmail.com>
2024-04-09 14:02:44 +0530

    seocnd

--- api/index.js
+++ api/index.js
@@ -1,19 +1,28 @@
-import  express  from "express";
+import express from "express";
 import mongoose from "mongoose";
-import dotenv from 'dotenv'
+import dotenv from "dotenv";
+import userRouter from './routes/user.route.js';
+import authRouter from './routes/auth.route.js';
+// import  test  from "./controllers/user.controller.js";
 dotenv.config();
 
-mongoose.connect(process.env.MONGO).then(()=>{
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => {
+    console.log('connected to MONGODB');
+  })
+  .catch((error) => {
+    console.log(error);
+  });
 
-    console.log("connected to MONGODB")
-}).catch((error)=>{
-    console.log(error)
-})
+const app = express();
+//alows json as a input 
+app.use(express.json());
 
-const app=express();
+// let port = 3000;
+app.listen(3000, () => {
+  console.log('service runnning on 3000');
+});
 
-let port=3000
-app.listen(3000,()=>{
-
-    console.log(`service runnning on ${port}`)
-})
+app.use('/api/user',userRouter);
+app.use('/api/auth',authRouter);
