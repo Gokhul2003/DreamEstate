24f395c3a8324553ad1c72258ae30fe54cc02035
Gokhul2003 <thegokhul.kooranchi@gmail.com>
2024-04-11 12:51:00 +0530

    api commit

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,9 +1,15 @@
-import User from "../models/user.model.js"
+import User from "../models/user.model.js";
+import { errorHandler } from "../utils/error.js";
+import bcyrptjs from "bcryptjs";
+export const signup = async (req, res,next) => {
+  const { username, email, password } = req.body;
+  const hashedPassword = bcyrptjs.hashSync(password, 10);
+  const newUser = new User({ username, email, password: hashedPassword });
+  try {
+    await newUser.save();
+    res.status(201).json("user created sucessfully");
+  } catch (error) {
+    next(error);
+  }
 
-export const signup = async (req,res)=>{
-
-    const{username, email, password}=req.body
-    const newUser=new User({username,email,password});
-        await newUser.save()
-        res.status(201).json("user created succefully")
 }
