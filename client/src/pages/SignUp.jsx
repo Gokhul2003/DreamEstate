a28b1462b390c75507bc1964dd595e42e8811c93
Gokhul2003 <thegokhul.kooranchi@gmail.com>
2024-04-15 23:15:06 +0530

    signup function

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,21 +1,33 @@
-import React from "react";
+import { useState } from "react";
 import { Link } from "react-router-dom";
-const SignUp = () => {
+export default function SignUp() {
+  const [formData, setFormData] = useState({});
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const handleChange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.id]: e.target.value,
+    });
+  };
+
   return (
     <div classname="p-3 max-w-lg mx-auto">
       <h1 className="text-3xl text-center font-semibold my-7">Sign Up</h1>
-      <form className="flex flex-col gap-4">
+      <form  onSubmit={handleSubmit} className="flex flex-col gap-4">
         <input
           type="text"
           placeholder="username"
           className="border p-3 rounded-lg"
           id="username"
+          onChange={handleChange}
         />
         <input
           type="email"
           placeholder="email"
           className="border p-3 rounded-lg"
           id="email"
+          onChange={handleChange}
         />
 
         <input
@@ -23,10 +35,9 @@ const SignUp = () => {
           placeholder="password"
           className="border p-3 rounded-lg"
           id="password"
+          onChange={handleChange}
         />
-        <button 
-        className="bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80"
-        >
+        <button className="bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80">
           SignUp
         </button>
       </form>
@@ -38,6 +49,4 @@ const SignUp = () => {
       </div>
     </div>
   );
-};
-
-export default SignUp;
+}
