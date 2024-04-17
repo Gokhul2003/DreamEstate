7a890777b55ae3f44453491891281026660bc4f1
Gokhul2003 <thegokhul.kooranchi@gmail.com>
2024-04-17 23:58:48 +0530

    route

--- api/routes/auth.route.js
+++ api/routes/auth.route.js
@@ -4,7 +4,7 @@ import { signup } from '../controllers/auth.controller.js';
 const router = express.Router();
 
 router.post('/signup', signup);
-// router.post('/signin', signin);
+router.post('/signin', signin);
 // router.post('/google', google);
 // router.get('/signout', signOut)
 
