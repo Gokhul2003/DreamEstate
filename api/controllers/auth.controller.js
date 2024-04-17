1ba7c026ade9f00cd5c21fb3da840ec8d9d43a1b
Gokhul2003 <thegokhul.kooranchi@gmail.com>
2024-04-18 00:26:46 +0530

    signin

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,6 +1,7 @@
 import User from "../models/user.model.js";
 import { errorHandler } from "../utils/error.js";
 import bcyrptjs from "bcryptjs";
+import jwt from 'jsonwebtoken';
 export const signup = async (req, res,next) => {
   const { username, email, password } = req.body;
   const hashedPassword = bcyrptjs.hashSync(password, 10);
@@ -22,8 +23,10 @@ export const signin = async (req, res, next) => {
     if (!validUser) return next(errorHandler(404, 'User not found!'));
     const validPassword = bcryptjs.compareSync(password, validUser.password);
     if (!validPassword) return next(errorHandler(401, 'Wrong credentials!'));
+    //this id is created in mongodb which is unique
     const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET);
     const { password: pass, ...rest } = validUser._doc;
+    //saving the token in cookie
     res
       .cookie('access_token', token, { httpOnly: true })
       .status(200)
