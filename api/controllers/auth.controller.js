bcbc5f29ce9322401e51c078b571e3c25c1208f8
Gokhul2003 <thegokhul.kooranchi@gmail.com>
2024-04-27 20:12:30 +0530

    commit

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -21,14 +21,14 @@ export const signin = async (req, res, next) => {
   try {
     const validUser = await User.findOne({ email });
     if (!validUser) return next(errorHandler(404, 'User not found!'));
-    const validPassword = bcryptjs.compareSync(password, validUser.password);
+    const validPassword = bcyrptjs.compareSync(password, validUser.password);
     if (!validPassword) return next(errorHandler(401, 'Wrong credentials!'));
     //this id is created in mongodb which is unique
     const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET);
     const { password: pass, ...rest } = validUser._doc;
     //saving the token in cookie
     res
-      .cookie('access_token', token, { httpOnly: true })
+      .cookie('access_token', token, { httpOnly: true })//no third pary can acess this coookie
       .status(200)
       .json(rest);
   } catch (error) {
