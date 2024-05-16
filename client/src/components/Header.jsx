0b1e0afd57e26f381168b0dee24ccf8641bffc26
Gokhul2003 <thegokhul.kooranchi@gmail.com>
2024-05-16 20:28:02 +0530

    profile page ui

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,7 +1,9 @@
 import React from "react";
 import { FaSearch } from "react-icons/fa";
 import { Link, useNavigate } from "react-router-dom";
+import { useSelector } from "react-redux";
 const Header = () => {
+  const {currentUser}=useSelector(state=>state.user)
   return (
     <header className="bg-slate-200 shadow-md">
       <div className="flex justify-between items-center max-w-6xl mx-auto p-3">
@@ -30,10 +32,16 @@ const Header = () => {
               About
             </li>
           </Link>
-          <Link to="/Sign-in">
-            <li className=" sm:inline text-slate-700 hover:underline">
-              Sign in
-            </li>
+          <Link to='/profile'>
+            {currentUser ? (
+              <img
+                className='rounded-full h-7 w-7 object-cover'
+                src={currentUser.avatar}
+                alt='profile'
+              />
+            ) : (
+              <li className=' text-slate-700 hover:underline'> Sign in</li>
+            )}
           </Link>
         </ul>
       </div>
