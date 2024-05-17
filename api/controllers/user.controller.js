7509881bbb0c68211d2e8f80ae19b3d06322d219
Gokhul2003 <thegokhul.kooranchi@gmail.com>
2024-05-18 00:03:05 +0530

    m

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -9,34 +9,34 @@ export const test = (req, res) => {
   });
 };
 
-// export const updateUser = async (req, res, next) => {
-//   if (req.user.id !== req.params.id)
-//     return next(errorHandler(401, 'You can only update your own account!'));
-//   try {
-//     if (req.body.password) {
-//       req.body.password = bcryptjs.hashSync(req.body.password, 10);
-//     }
+export const updateUser = async (req, res, next) => {
+  if (req.user.id !== req.params.id)
+    return next(errorHandler(401, 'You can only update your own account!'));
+  try {
+    if (req.body.password) {
+      req.body.password = bcryptjs.hashSync(req.body.password, 10);
+    }
 
-//     const updatedUser = await User.findByIdAndUpdate(
-//       req.params.id,
-//       {
-//         $set: {
-//           username: req.body.username,
-//           email: req.body.email,
-//           password: req.body.password,
-//           avatar: req.body.avatar,
-//         },
-//       },
-//       { new: true }
-//     );
+    const updatedUser = await User.findByIdAndUpdate(
+      req.params.id,
+      {
+        $set: {
+          username: req.body.username,
+          email: req.body.email,
+          password: req.body.password,
+          avatar: req.body.avatar,
+        },
+      },
+      { new: true }
+    );
 
-//     const { password, ...rest } = updatedUser._doc;
+    const { password, ...rest } = updatedUser._doc;
 
-//     res.status(200).json(rest);
-//   } catch (error) {
-//     next(error);
-//   }
-// };
+    res.status(200).json(rest);
+  } catch (error) {
+    next(error);
+  }
+};
 
 // export const deleteUser = async (req, res, next) => {
 //   if (req.user.id !== req.params.id)
--- api/index.js
+++ api/index.js
@@ -3,6 +3,7 @@ import mongoose from "mongoose";
 import dotenv from "dotenv";
 import userRouter from './routes/user.route.js';
 import authRouter from './routes/auth.route.js';
+import cookieParser from "cookie-parser";
 // import  test  from "./controllers/user.controller.js";
 dotenv.config();
 
@@ -19,6 +20,7 @@ const app = express();
 //alows json as a input 
 app.use(express.json());
 
+app.use(cookieParser())
 // let port = 3000;
 app.listen(3000, () => {
   console.log('service runnning on 3000');
