3391e579028c292246093259f2cdcd5490d2f976
Gokhul2003 <thegokhul.kooranchi@gmail.com>
2024-05-28 23:59:36 +0530

    m

--- api/controllers/listing.controller.js
+++ api/controllers/listing.controller.js
@@ -68,38 +68,38 @@ export const getListings = async (req, res, next) => {
     const startIndex = parseInt(req.query.startIndex) || 0;
     let offer = req.query.offer;
 
-    if (offer === undefined || offer === 'false') {
+    if (offer === undefined || offer === "false") {
       offer = { $in: [false, true] };
     }
 
     let furnished = req.query.furnished;
 
-    if (furnished === undefined || furnished === 'false') {
+    if (furnished === undefined || furnished === "false") {
       furnished = { $in: [false, true] };
     }
 
     let parking = req.query.parking;
 
-    if (parking === undefined || parking === 'false') {
+    if (parking === undefined || parking === "false") {
       parking = { $in: [false, true] };
     }
 
     let type = req.query.type;
 
-    if (type === undefined || type === 'all') {
-      type = { $in: ['sale', 'rent'] };
+    if (type === undefined || type === "all") {
+      type = { $in: ["sale", "rent"] };
     }
 
-    const searchTerm = req.query.searchTerm || '';
+    const searchTerm = req.query.searchTerm || "";
 
-    const sort = req.query.sort || 'createdAt';
+    const sort = req.query.sort || "createdAt";
 
-    const order = req.query.order || 'desc';
+    const order = req.query.order || "desc";
 
     //this route is fetching the data which matches the query if furnished is true in the query then it will show
     //also in the name we've added regex tu search all sentence and words and to avoid the caps we've  provided '$options:i'
     const listings = await Listing.find({
-      name: { $regex: searchTerm, $options: 'i' },
+      name: { $regex: searchTerm, $options: "i" },
       offer,
       furnished,
       parking,
