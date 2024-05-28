a91de8d908958b18bbf924a0b780de94b349f486
Gokhul2003 <thegokhul.kooranchi@gmail.com>
2024-05-28 12:55:14 +0530

    listing page with  email  functionality added

--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -4,6 +4,7 @@ import {
   getUserListings,
   test,
   updateUser,
+  getUser,
 } from "../controllers/user.controller.js";
 import { verifyToken } from "../utils/verifyUser.js";
 
@@ -13,4 +14,6 @@ router.get("/test", test);
 router.post("/update/:id", verifyToken, updateUser);
 router.delete("/delete/:id", verifyToken, deleteUser);
 router.get("/listings/:id", verifyToken, getUserListings);
+router.get('/:id', verifyToken, getUser)
+
 export default router;
