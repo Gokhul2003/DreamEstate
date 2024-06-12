998f7c341cfe235f2267b2da269fd80f6afd1c6c
Gokhul2003 <thegokhul.kooranchi@gmail.com>
2024-06-12 10:28:53 +0530

    deploy code

--- api/index.js
+++ api/index.js
@@ -1,39 +1,47 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
-import userRouter from './routes/user.route.js';
-import authRouter from './routes/auth.route.js';
+import userRouter from "./routes/user.route.js";
+import authRouter from "./routes/auth.route.js";
 import cookieParser from "cookie-parser";
-import listingRouter from './routes/listing.route.js';
+import listingRouter from "./routes/listing.route.js";
 // import  test  from "./controllers/user.controller.js";
+import path from "path";
 dotenv.config();
 
 mongoose
   .connect(process.env.MONGO)
   .then(() => {
-    console.log('connected to MONGODB');
+    console.log("connected to MONGODB");
   })
   .catch((error) => {
     console.log(error);
   });
 
+const __dirname = path.resolve();
+
 const app = express();
-//alows json as a input 
+
+//alows json as a input
 app.use(express.json());
 
-app.use(cookieParser())
+app.use(cookieParser());
 // let port = 3000;
 app.listen(3000, () => {
-  console.log('service runnning on 3000');
+  console.log("service runnning on 3000");
 });
 
-app.use('/api/user',userRouter);
-app.use('/api/auth',authRouter);
-app.use('/api/listing',listingRouter);
+app.use("/api/user", userRouter);
+app.use("/api/auth", authRouter);
+app.use("/api/listing", listingRouter);
 
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
+});
+app.use(express.static());
 app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
-  const message = err.message || 'Internal Server Error';
+  const message = err.message || "Internal Server Error";
   return res.status(statusCode).json({
     success: false,
     statusCode,
